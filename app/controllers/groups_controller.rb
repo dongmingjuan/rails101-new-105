a8b00b5ba6aaab79b4e3d8115ad7c9e07d4b2f65
dongmingjuan <d387918156@gmail.com>
2017-08-05 08:01:29 +0800

    only member can write a essay

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -35,6 +36,20 @@ class GroupsController < ApplicationController
 
     @group.destroy
     redirect_to groups_path
+  end
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+    end
+    redirect_to group_path(@group)
+  end
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+    end
+    redirect_to group_path(@group)
   end
   private
   def find_group_and_check_pemission
